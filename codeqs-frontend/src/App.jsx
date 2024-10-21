df87f892b09b91465dd8abfd46cee278ee5caf84
Anjana George <anjana7581@gmail.com>
2024-10-21 16:19:57 +0530

    home page, routes added

--- codeqs-frontend/src/App.jsx
+++ codeqs-frontend/src/App.jsx
@@ -1,9 +1,15 @@
+import Home from "./pages/Home/Home"
 import Login from "./pages/Login/Login"
+import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Login/>
+      {/* <Login/> */}
+      <Routes>
+        <Route path='/' element ={<Home/>}/>
+        <Route path='/login' element ={<Login/>}/>
+      </Routes>
     </div>
   )
 }
