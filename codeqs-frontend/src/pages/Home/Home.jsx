f9ec235cf6cb7d9f9f788ca12514deff236c92b3
Anjana George <anjana7581@gmail.com>
2024-10-21 21:44:19 +0530

    added footer

--- codeqs-frontend/src/pages/Home/Home.jsx
+++ codeqs-frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
+import Footer from '../../Components/Footer/Footer'
 import Navbar from '../../Components/Navbar/Navbar'
 import './Home.css'
 const Home = () => {
   return (
     <div>
       <Navbar/>
-
+      <Footer/>
     </div>
   )
 }
