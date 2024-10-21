818ccecdba9c1bb6a5addfe4b00ee2af53f11f79
SwathiSajeshM <swathimknr@gmail.com>
2024-10-21 23:18:11 +0530

    navbar responsive

--- codeqs-frontend/src/Components/Navbar/Navbar.jsx
+++ codeqs-frontend/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,54 @@
-import logo from '../../assets/logo.png'
-import './Navbar.css'
-import { FiSearch } from "react-icons/fi"; // Import arrow icons
+import React, { useState } from 'react';
+import './Navbar.css';
+import { CiSearch } from "react-icons/ci";
+import logo from '../../assets/logo.png';
 
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-const Nav = () => {
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <div className="navbar">
-        <div className="navbar-left">
+    <header className="navbar">
+      <div>
         <img src={logo} alt="" className='logo-img' />
-        <>
-        <input type="search" name="" id="" />
-          <FiSearch className='search-icon' />
-        </>
         </div>
-    </div>
-  )
-}
+      <div className="search-bar">
+        <input type="text" placeholder="Find your next skill to master..." />
+        <CiSearch className="search-icon" />
+      </div>
+      <nav className={`nav-links ${isMobileMenuOpen ? 'open' : ''}`}>
+      <div className="mob-search-bar">
+<input type="text" placeholder="Find your next skill to master..." />
+<CiSearch className="mob-search-icon" />
+</div>
+        
+        <a href="#home">Home</a>
+        <a href="#courses">Courses</a>
+        <a href="#services">Services</a>
+        <a href="#blog">Blog</a>
+        <a href="#about">About Us</a>
+        <a href="#contact">Contact us</a>
 
-export default Nav
+        <button className="signup-btn">Sign up</button>
+       
+      </nav>
+  
+     
+      <div className="hamburger" onClick={toggleMobileMenu}>
+        {isMobileMenuOpen ? (     
+          <span className="close-icon">&times;</span> 
+        ) : (
+          
+          <span className="hamburger-icon">&#9776;</span>
+        
+     
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
