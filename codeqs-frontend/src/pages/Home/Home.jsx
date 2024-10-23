786327c05da25ec49ffac10a5c74ebd63bcd904b
ananda krishnan <ebinebin54@gmail.com>
2024-10-23 18:00:05 +0530

    private protection added for routes

--- codeqs-frontend/src/pages/Home/Home.jsx
+++ codeqs-frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
-import Footer from '../../Components/Footer/Footer'
-import Navbar from '../../Components/Navbar/Navbar'
-import './Home.css'
-const Home = () => {
-  return (
-    <div>
-      <Navbar/>
-      <Footer/>
-    </div>
-  )
-}
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer';
+import './Home.css';
 
-export default Home
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Welcome to the Home Page!</h1>
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
