3904d72350cfc456b66e0dab2b121a765596754c
Anjana George <anjana7581@gmail.com>
2024-10-23 12:05:47 +0530

    Home page updates

--- codeqs-frontend/src/pages/Home/Home.jsx
+++ codeqs-frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,23 @@
-import Footer from '../../Components/Footer/Footer'
-import Navbar from '../../Components/Navbar/Navbar'
-import './Home.css'
+import Banner from '../../Components/Banner/Banner';
+import Footer from '../../Components/Footer/Footer';
+import Navbar from '../../Components/Navbar/Navbar';
+import Fcourse from '../../Components/Featuredcourse/Fcourse';
+
+import './Home.css';
+
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <Banner/>
+      <Fcourse/>
+     
+      
       <Footer/>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+
+
+export default Home;
