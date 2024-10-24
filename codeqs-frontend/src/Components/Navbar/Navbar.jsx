6ff3f971ca16aab1e1f9ae649db1e51475ccfcff
Sreeharsh k <sreeharshk92@gmail.com>
2024-10-24 16:53:07 +0530

    chore: changed navbar

--- codeqs-frontend/src/Components/Navbar/Navbar.jsx
+++ codeqs-frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import { Link, useNavigate } from 'react-router-dom'; 
+import { Link, useNavigate } from 'react-router-dom';
 
-import  { useState } from 'react';
+import { useState } from 'react';
 import './Navbar.css';
 import { CiSearch } from "react-icons/ci";
 import logo from '../../assets/logo.png';
@@ -12,27 +12,27 @@ const Navbar = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleSignin = () => {
-    navigate('/login'); 
+    navigate('/login');
   }
 
   return (
     <header className="navbar">
       <div>
         <img src={logo} alt="" className='logo-img' />
-        </div>
+      </div>
       <div className="search-bar">
         <input type="text" placeholder="Find your next skill to master..." />
         <CiSearch className="search-icon" />
       </div>
       <nav className={`nav-links ${isMobileMenuOpen ? 'open' : ''}`}>
-      <div className="mob-search-bar">
-<input type="text" placeholder="Find your next skill to master..." />
-<CiSearch className="mob-search-icon" />
-</div>
-        
+        <div className="mob-search-bar">
+          <input type="text" placeholder="Find your next skill to master..." />
+          <CiSearch className="mob-search-icon" />
+        </div>
+
         <Link to="/">Home</Link>
         <Link to="/courses">Courses</Link>
         <Link to="/blog">Blog</Link>
@@ -47,18 +47,18 @@ const Navbar = () => {
         <a href="#contact">Contact us</a> */}
 
         <button onClick={handleSignin} className="signup-btn">Sign up</button>
-       
+
       </nav>
-  
-     
+
+
       <div className="hamburger" onClick={toggleMobileMenu}>
-        {isMobileMenuOpen ? (     
-          <span className="close-icon">&times;</span> 
+        {isMobileMenuOpen ? (
+          <span className="close-icon">&times;</span>
         ) : (
-          
+
           <span className="hamburger-icon">&#9776;</span>
-        
-     
+
+
         )}
       </div>
     </header>
