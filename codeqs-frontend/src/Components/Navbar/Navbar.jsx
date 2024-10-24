8370de7b13370957e36440c1980e2334918e1f15
Anjana George <anjana7581@gmail.com>
2024-10-24 14:39:05 +0530

    navbar re added

--- codeqs-frontend/src/Components/Navbar/Navbar.jsx
+++ codeqs-frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'; 
 
-import { useState } from 'react';
+import  { useState } from 'react';
 import './Navbar.css';
 import { CiSearch } from "react-icons/ci";
 import logo from '../../assets/logo.png';
@@ -12,35 +12,27 @@ const Navbar = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
-  const navigate = useNavigate();
+  const navigate = useNavigate(); 
 
   const handleSignin = () => {
-    navigate('/login');
+    navigate('/login'); 
   }
 
   return (
-<header>
-
-
-    <div>
-    <img src={logo} alt="" className='logo-img' />
-  </div>
-
-<div className="search-bar">
-<input type="text" placeholder="Find your next skill to master..." />
-<CiSearch className="search-icon" />
-</div>
-
-<nav className={`nav-links ${isMobileMenuOpen ? 'open' : ''}`}>
-
-
-
-<div className="mob-search-bar">
-          <input type="text" placeholder="Find your next skill to master..." />
-          <CiSearch className="mob-search-icon" />
+    <header className="navbar">
+      <div>
+        <img src={logo} alt="" className='logo-img' />
         </div>
-
-
+      <div className="search-bar">
+        <input type="text" placeholder="Find your next skill to master..." />
+        <CiSearch className="search-icon" />
+      </div>
+      <nav className={`nav-links ${isMobileMenuOpen ? 'open' : ''}`}>
+      <div className="mob-search-bar">
+<input type="text" placeholder="Find your next skill to master..." />
+<CiSearch className="mob-search-icon" />
+</div>
+        
         <Link to="/">Home</Link>
         <Link to="/courses">Courses</Link>
         <Link to="/blog">Blog</Link>
@@ -48,32 +40,29 @@ const Navbar = () => {
         <Link to="/contactus">Contact Us</Link>
 
 
+        {/* <li href="#courses">Courses</li>
+        <a href="#services">Services</a>
+        <a href="#blog">Blog</a>
+        <a href="#about">About Us</a>
+        <a href="#contact">Contact us</a> */}
+
         <button onClick={handleSignin} className="signup-btn">Sign up</button>
-</nav>
-
-        <div className="hamburger" onClick={toggleMobileMenu}>
-        {isMobileMenuOpen ? (
-          <span className="close-icon">&times;</span>
+       
+      </nav>
+  
+     
+      <div className="hamburger" onClick={toggleMobileMenu}>
+        {isMobileMenuOpen ? (     
+          <span className="close-icon">&times;</span> 
         ) : (
-
+          
           <span className="hamburger-icon">&#9776;</span>
-
-
+        
+     
         )}
       </div>
-
-
-
-
-
-
-
-</header>
-
-
-
+    </header>
   );
-
 };
 
-export default Navbar
+export default Navbar;
